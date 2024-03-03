d6e55f6c8846d7616b946e4f2ff767226b1c0488
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-03-03 18:36:08 +0100

    Initial commit

---
+++ resources/views/admin/types/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-white mt-5">Inserisci nuovo progetto</h1>
+            </div>
+            <div class="col-12">
+                <form class="mt-5" action="{{ route('admin.types.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group my-3">
+                        <label for="nome" class="control-label text-white">nome</label>
+                        <input type="text" name="nome" id="nome" placeholder="Inserisci il nome della tipologia"
+                            class="form-control @error('nome') is-invalid @enderror" value="{{ old('nome') }}" required>
+                        @error('nome')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <label class="control-label text-white">Selezione tecnologia</label>
+                        <div class="my-3">
+                            @foreach ($technologies as $technology)
+                                <div class="form-check-inline">
+                                    <input type="checkbox" name="technologies[]" id="" class="form-check-input"
+                                        value="{{ $technology->id }}" @checked(is_array(old('technologies')) && in_array($technology->id, old('technologies')))>
+                                    <label for=""
+                                        class="fomr-check-label text-white">{{ $technology->nome }}</label>
+                                </div>
+                            @endforeach
+                        </div>
+
+                    <div class="form-group my-3">
+                        <label for="descrizione" class="control-label text-white">Descrizione</label>
+                        <textarea name="descrizione" id="descrizione" class="form-control @error('descrizione') is-invalid @enderror"
+                            placeholder="Inserisci la descrizione della tipologia" cols="50" rows="10" required>{{ old('descrizione') }}</textarea>
+                        @error('descrizione')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group my-3">
+                        <button class="btn btn-success" type="submit">Salva</button>
+                    </div>
+                    <a href="{{ route('admin.types.index') }}" class="btn btn-primary float-end">Torna alle tipologie
+                    </a>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content') 
+    <h1 class="text-center mt-3">Titolo: {{$project->title}}</h1>
+    <img src="{{asset('/storage/'.$project->project_image)}}" alt="{{$project->title}}" class="img-fluid">
+    <h3 class="text-center mt-5">Descrizione:</h3>
+    <p class="text-center">{{$project->description}}</p>
+    <p class="text-center">Categoria: {{$project->type_id ? $project->type->nome : 'Nessuna categoria'}}</p>
+    <p>
+        @forelse ($project->technologies as $technology)
+            #{{ $technology->nome }}
+        @empty
+            Non è stata assegnata nessuna tecnologia
+        @endforelse
+    </p>
+    <p class="text-end me-5 mt-4">Data di consegna: {{$project->end_date}}</p>
+
+    <a href="{{route('admin.projects.edit', $project->id)}}" class="ms-5">
+        <button class="btn btn-sm btn-primary">Modifica</button>
+    </a>
+
+        <form action="{{route('admin.projects.destroy', $project->id)}}" method="post" onsubmit="return confirm('Sicuro di volere cancellare questo progetto?')">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm btn-danger ms-5">Elimina</button>
+        </form>
+@endsection
